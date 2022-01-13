60c5104ddd8a50003f0b59f577005e54e657022a
srk-333 <sauravmk333@gmail.com>
2022-01-13 19:18:14 +0530

    [Saurav]Added Welcome message

---
+++ LineComparisionComputation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisionComputation
+{
+    public class Program
+    {
+        //Main method /Entry Point of program
+        static void Main(string[] args)
+        {
+            //Showing message on console
+            Console.WriteLine("Welcome to Line Comparision Computation Program");
+            Console.ReadLine(); 
+        }
+    }
+}
