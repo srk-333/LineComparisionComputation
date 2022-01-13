8a127fd442a23df9d6c3b9cfca8512e512561487
srk-333 <sauravmk333@gmail.com>
2022-01-13 22:21:16 +0530

    [Saurav]Added program to Compare Two Lines

--- LineComparisionComputation/LineEquality.cs
+++ LineComparisionComputation/LineEquality.cs
@@ -8,10 +8,12 @@ namespace LineComparisionComputation
 {
     public class LineEquality
     {
-
-
+        /* Uc - 3 Implementation
+         * here we are going to compare two lines length using compareTo method
+         * and show appropriate outcome to end User.
+         */
         //instance variable to store distance of given co-ordinates.
-        static double distance1, distance2, distance3;
+        static double distance1, distance2;
 
         // Method for creating co-ordinates
         public static void CheckLine()
@@ -23,24 +25,21 @@ namespace LineComparisionComputation
             distance1 = lc1.Length();
             LineComputation lc2 = new LineComputation(3.2, 4.5, 2.4, 4.2);
             distance2 = lc2.Length();
-            LineComputation lc3 = new LineComputation(2.3, 3.4, 1.5, 2.5);
-            distance3 = lc3.Length();
             Console.WriteLine("Length of Line1: " + distance1);
             Console.WriteLine("Length of Line2: " + distance2);
-            Console.WriteLine("Length of Line3: " + distance3);
 
             //checking equality of lines using equals method 
-            if (distance1.Equals(distance2))
+            if (distance1.CompareTo(distance2) > 0)
             {
-                Console.WriteLine("lines one and two are equal");
+                Console.WriteLine("line one is greater than line two");
             }
-            else if (distance2.Equals(distance3))
+            else if (distance1.CompareTo(distance2) < 0)
             {
-                Console.WriteLine("lines two and three are equal");
+                Console.WriteLine("line one is less then line Two");
             }
-            else if (distance1.Equals(distance3))
+            else if (distance1.CompareTo(distance2) == 0)
             {
-                Console.WriteLine("lines one and three are equal");
+                Console.WriteLine("Both Lines are Equals");
             }
             else
             {
