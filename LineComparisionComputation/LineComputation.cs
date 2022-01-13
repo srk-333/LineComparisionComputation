e7d0c07ebd9c578da7753c3f4bca7e60a9ad7482
srk-333 <sauravmk333@gmail.com>
2022-01-13 20:49:39 +0530

    [Saurav]Added Line distance Check program

---
+++ LineComparisionComputation/LineComputation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisionComputation
+{
+    public class LineComputation
+    {
+        double pointX1, pointY1, pointX2, pointY2;
+        public LineComputation(double pointX1,double pointY1,double pointX2,double pointY2)
+        { 
+            this.pointX1 = pointX1;
+            this.pointY1 = pointY1;
+            this.pointX2 = pointX2;
+            this.pointY2 = pointY2;
+        }
+        public double Length()
+        {
+            return Math.Sqrt((pointX2 - pointX1) * (pointX2 - pointX1) + (pointY2 - pointY1) * (pointY2 - pointY1));
+        }
+    }
+}
--- LineComparisionComputation/Program.cs
+++ LineComparisionComputation/Program.cs
@@ -9,10 +9,21 @@ namespace LineComparisionComputation
     public class Program
     {
         //Main method /Entry Point of program
+        static double distance1, distance2;
+        public static void CheckLine()
+        {
+            LineComputation lc1 = new LineComputation(2.3, 3.4, 1.5, 2.5);
+            distance1 = lc1.Length();
+            LineComputation lc2 = new LineComputation(3.2, 4.5, 2.4, 4.2);
+            distance2 = lc2.Length();
+            Console.WriteLine(distance1);
+            Console.WriteLine(distance2);
+        }      
         static void Main(string[] args)
         {
             //Showing message on console
             Console.WriteLine("Welcome to Line Comparision Computation Program");
+            Program.CheckLine();
             Console.ReadLine(); 
         }
     }
