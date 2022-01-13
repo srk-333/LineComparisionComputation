1d1c5d34af6937f8eda0f0212222834e0030df4b
srk-333 <sauravmk333@gmail.com>
2022-01-13 21:03:13 +0530

    [Saurav]Added Comment in program

--- LineComparisionComputation/LineComputation.cs
+++ LineComparisionComputation/LineComputation.cs
@@ -8,7 +8,9 @@ namespace LineComparisionComputation
 {
     public class LineComputation
     {
+        //Instace Variables.
         double pointX1, pointY1, pointX2, pointY2;
+        //Constructor initilalizing values through pass by reference.
         public LineComputation(double pointX1,double pointY1,double pointX2,double pointY2)
         { 
             this.pointX1 = pointX1;
@@ -16,8 +18,10 @@ namespace LineComparisionComputation
             this.pointX2 = pointX2;
             this.pointY2 = pointY2;
         }
+        //Method to compute length of given co-ordinates of geometery..
         public double Length()
         {
+            //returning double type value after computation.
             return Math.Sqrt((pointX2 - pointX1) * (pointX2 - pointX1) + (pointY2 - pointY1) * (pointY2 - pointY1));
         }
     }
--- LineComparisionComputation/Program.cs
+++ LineComparisionComputation/Program.cs
@@ -8,22 +8,28 @@ namespace LineComparisionComputation
 {
     public class Program
     {
-        //Main method /Entry Point of program
+        //instance variable to store distance of given co-ordinates.
         static double distance1, distance2;
+
+        // Method for creating co-ordinates
         public static void CheckLine()
         {
+            /* passing values to constructor while creating class object.
+             * calling Length method to get length of co-ordinates.
+             */
             LineComputation lc1 = new LineComputation(2.3, 3.4, 1.5, 2.5);
             distance1 = lc1.Length();
             LineComputation lc2 = new LineComputation(3.2, 4.5, 2.4, 4.2);
             distance2 = lc2.Length();
             Console.WriteLine(distance1);
             Console.WriteLine(distance2);
-        }      
+        }
+        //Main method /Entry Point of program
         static void Main(string[] args)
         {
             //Showing message on console
             Console.WriteLine("Welcome to Line Comparision Computation Program");
-            Program.CheckLine();
+            Program.CheckLine();  // calling static method of program class.
             Console.ReadLine(); 
         }
     }
